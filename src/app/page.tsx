5b8331e182c99f7ea6422416301c8b840eadc26c
jrmaia94 <euilis.jr@gmail.com>
2025-03-10 19:14:17 -0300

    feat: add initial restaurant page and menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 const HomePage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+  return <h1 className="text-3xl">Hello world</h1>;
 };
 
 export default HomePage;
