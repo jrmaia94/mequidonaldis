022f6dca01e94b94f9345ad9b6c0a45685bbd8c4
jrmaia94 <euilis.jr@gmail.com>
2025-03-10 21:19:33 -0300

    feat: add product header components

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductHeader from "./components/product-header";
+
+interface ProductPageProps {
+  params: Promise<{ slug: string; productId: string }>
+}
+
+const ProductPage = async ({ params }: ProductPageProps) => {
+  const { productId, slug } = await params;
+  console.log(slug);
+  const product = await db.product.findUnique({ where: { id: productId } });
+  if (!product) return notFound();
+
+  return (
+    <div>
+      <ProductHeader product={product} />
+    </div>
+  )
+}
+
+export default ProductPage;
--- src/app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-const ProductPage = () => {
-  return <Button>Clique-me</Button>;
-};
-
-export default ProductPage;
